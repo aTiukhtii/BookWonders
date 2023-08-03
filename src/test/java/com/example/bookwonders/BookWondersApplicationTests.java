92d93dc44451be30691c1e2e6856aa6a5b38b13b
Artem <cr7realartem@gmail.com>
2023-08-03 19:01:08 +0300

    first commit

---
+++ src/test/java/com/example/bookwonders/BookWondersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookwonders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BookWondersApplicationTests.class)
+class BookWondersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
