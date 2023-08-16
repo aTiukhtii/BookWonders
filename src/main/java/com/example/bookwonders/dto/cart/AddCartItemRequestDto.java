8a5e85597c95ed0e23d8859195952488127a60d3
Artem <cr7realartem@gmail.com>
2023-08-16 13:50:12 +0300

    fix annotation

--- src/main/java/com/example/bookwonders/dto/cart/AddCartItemRequestDto.java
+++ src/main/java/com/example/bookwonders/dto/cart/AddCartItemRequestDto.java
@@ -1,15 +1,15 @@
 package com.example.bookwonders.dto.cart;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AddCartItemRequestDto {
-    @NotBlank
+    @NotNull
     @Positive
     private Long bookId;
-    @NotBlank
+    @NotNull
     @Positive
-    private int quantity;
+    private Integer quantity;
 }
